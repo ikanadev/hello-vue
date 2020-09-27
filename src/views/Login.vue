fe1a5b394ebe55ecb1179cc4af24a7c6cae3f779
Kevv <vargaskevv@gmail.com>
2020-09-27 10:57:01 -0400

    Added tailwind support

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-center items-center">
+    <div class="rounded shadow-md p-2">
+      Hello Tailwind
+      <p>{{ msg }}</p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class Login extends Vue {
+  msg = 'Hello World'
+}
+</script>
